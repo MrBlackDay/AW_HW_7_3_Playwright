b76b5c374a01e17255544f21df046ceab3704757
Dima Lis <iselis777888999@gmail.com>
2024-07-06 17:17:21 +0300

    delete user data

--- user.js
+++
@@ -1,7 +0,0 @@
-const invalidName = "vasya@mail.com";
-const invalidPassword = "qwerty1234";
-
-const validName = "mr.b1ack.day@yandex.ru";
-const validPassword = "qweasdzxc1234GOOD";
-
-module.exports = { invalidName, invalidPassword, validName, validPassword };
